c7e43c1dae9ea9a2db0331d9c21002bb526abce6
Kevin Hartman <kevin@hart.mn>
2019-11-10 02:22:31 -0500

    Clean up implementation of TupleSerialization.h.

--- include/Support/TupleSerialization.h
+++ include/Support/TupleSerialization.h
@@ -8,90 +8,77 @@
 namespace serializer_internal {
 // TODO: rewrite this if possible to avoid duplicating everything for const overloads
 template<support::Endian Endian, typename Visitor, typename Stream, bool ShouldSwap = Endian != support::Endian::ignore && Endian != support::HostEndian>
-struct TupleTraverser {
+struct TupleVisitor {
     // TODO: add specializations for single byte scalars
     template<typename T, typename std::enable_if<std::is_scalar<T>::value && !ShouldSwap, int>::type = 0>
-    static void Write(T& field, Stream& output) {
+    static void Visit(T& field, Stream& output) {
         Visitor::VisitScalar(field, output);
     }
 
     template<typename T, typename std::enable_if<std::is_scalar<T>::value && !ShouldSwap, int>::type = 0>
-    static void Write(const T& field, Stream& output) {
+    static void Visit(const T& field, Stream& output) {
         Visitor::VisitScalar(field, output);
     }
 
     template<typename T, typename std::enable_if<std::is_scalar<T>::value && ShouldSwap, int>::type = 0>
-    static void Write(T& field, Stream& output) {
+    static void Visit(T& field, Stream& output) {
         Visitor::VisitSwappedScalar(field, output);
     }
 
     template<typename T, typename std::enable_if<std::is_scalar<T>::value && ShouldSwap, int>::type = 0>
-    static void Write(const T& field, Stream& output) {
+    static void Visit(const T& field, Stream& output) {
         Visitor::VisitSwappedScalar(field, output);
     }
 
     template<size_t I>
-    static void Write(std::array<char, I>& field, Stream& output) {
+    static void Visit(std::array<char, I>& field, Stream& output) {
         Visitor::VisitCharArray(field, output);
     }
 
     template<size_t I>
-    static void Write(const std::array<char, I>& field, Stream& output) {
+    static void Visit(const std::array<char, I>& field, Stream& output) {
         Visitor::VisitCharArray(field, output);
     }
 
     template<typename T, size_t I>
-    static void Write(std::array<T, I>& field, Stream& output) {
-        printf("array\n");
-
+    static void Visit(std::array<T, I>& field, Stream& output) {
         for (auto& element : field) {
-            Write(element, output);
+            Visit(element, output);
         }
-
-        printf("end array\n");
     }
 
     template<typename T, size_t I>
-    static void Write(const std::array<T, I>& field, Stream& output) {
-        printf("array\n");
-
+    static void Visit(const std::array<T, I>& field, Stream& output) {
         for (auto& element : field) {
-            Write(element, output);
+            Visit(element, output);
         }
-
-        printf("end array\n");
     }
 
     template<typename T, typename std::enable_if<!std::is_scalar<T>::value, int>::type = 0>
-    static void Write(const T& field, Stream& output) {
+    static void Visit(const T& field, Stream& output) {
         static_assert(std::is_scalar<T>::value, "Serializer only supports std::tuple and std::array containers and scalar elements.");
-        printf("non scalar;\n");
     }
 
     template<typename ... Ts, size_t ... Is>
-    static void Write(std::tuple<Ts...>& structure, Stream& output, std::index_sequence<Is...>) {
+    static void Visit(std::tuple<Ts...>& structure, Stream& output, std::index_sequence<Is...>) {
         using swallow = int[];
-        (void)swallow{ 1,
-                       (Write(std::get<Is>(structure), output), void(), int{})...
-        };
+        (void)swallow{(Visit(std::get<Is>(structure), output), int{})...};
     }
 
     template<typename ... Ts, size_t ... Is>
-    static void Write(const std::tuple<Ts...>& structure, Stream& output, std::index_sequence<Is...>) {
+    static void Visit(const std::tuple<Ts...>& structure, Stream& output, std::index_sequence<Is...>) {
         using swallow = int[];
-        (void)swallow{ 1,
-                       (Write(std::get<Is>(structure), output), void(), int{})...
-        };
+        (void)swallow{(Visit(std::get<Is>(structure), output), int{})...};
     }
 
     template<typename ... Ts>
-    static void Write(std::tuple<Ts...>& structure, Stream& output) {
-        Write(structure, output, std::make_index_sequence<sizeof...(Ts)>{});
+    static void Visit(std::tuple<Ts...>& structure, Stream& output) {
+        Visit(structure, output, std::make_index_sequence<sizeof...(Ts)>{});
     }
 
     template<typename ... Ts>
-    static void Write(const std::tuple<Ts...>& structure, Stream& output) {
-        Write(structure, output, std::make_index_sequence<sizeof...(Ts)>{});
+    static void Visit(const std::tuple<Ts...>& structure, Stream& output) {
+        Visit(structure, output, std::make_index_sequence<sizeof...(Ts)>{});
     }
 };
 
@@ -101,19 +88,16 @@ struct TupleElementWriter {
         T copy = field;
         support::EndianSwap(&copy);
         stream.write(reinterpret_cast<const char *>(&copy), sizeof(T));
-        printf("Write SwappedScalar]\n");
     }
 
     template <typename T>
     static void VisitScalar(const T& field, std::ostream& stream) {
         stream.write(reinterpret_cast<const char *>(&field), sizeof(T));
-        printf("Write Scalar\n");
     }
 
     template<size_t I>
     static void VisitCharArray(const std::array<char, I>& field, std::ostream& stream) {
         stream.write(field.data(), field.size());
-        printf("Write CharArray\n");
     }
 };
 
@@ -122,19 +106,16 @@ struct TupleElementReader {
     static void VisitSwappedScalar(T& field, std::istream& stream) {
         stream.read(reinterpret_cast<char *>(&field), sizeof(T));
         support::EndianSwap(&field);
-        printf("Read SwappedScalar]\n");
     }
 
     template <typename T>
     static void VisitScalar(T& field, std::istream& stream) {
         stream.read(reinterpret_cast<char *>(&field), sizeof(T));
-        printf("Read Scalar\n");
     }
 
     template<size_t I>
     static void VisitCharArray(std::array<char, I>& field, std::istream& stream) {
         stream.read(field.data(), field.size());
-        printf("Read CharArray\n");
     }
 };
 
@@ -145,7 +126,7 @@ namespace serializer {
 template<support::Endian Endian, typename Tuple, typename OStream, typename ...Ts>
 void SerializeTuple(const Tuple& structure, OStream& output) {
     using namespace serializer_internal;
-    TupleTraverser<Endian, TupleElementWriter, OStream>::Write(structure, output);
+    TupleVisitor<Endian, TupleElementWriter, OStream>::Visit(structure, output);
 }
 
 template <typename Tuple, typename OStream>
@@ -166,7 +147,7 @@ static void SerializeTuple(const Tuple& tuple, OStream& file, support::Endian en
 template<support::Endian Endian, typename Tuple, typename IStream, typename ...Ts>
 void DeserializeTuple(Tuple& structure, IStream& input) {
     using namespace serializer_internal;
-    TupleTraverser<Endian, TupleElementReader, IStream>::Write(structure, input);
+    TupleVisitor<Endian, TupleElementReader, IStream>::Visit(structure, input);
 }
 
 template <typename Tuple, typename IStream>
