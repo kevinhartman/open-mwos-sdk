b7b8bb327f74fa25bf9dddd1f412158ccf8eff91
Kevin Hartman <kevin@hart.mn>
2019-12-26 14:49:35 -0500

    [amips][Assembler] Move requires arg reading check to compile-time.
    This makes use of "if constexpr" within expression parsers (RType(), IType(), etc.) to conditionally compile in reading of arguments from the operands field. This works by introducing a sentinel (called "Arg") which, when provided as the numeric constant expression value for a parser template argument, is recognized by the parser function's corresponding "constexpr if" check. If the sentinel is provided, the corresponding operand will be read from the operands parser.
    
    For example:
    
      IType<0b101010, Arg, 0b11111, Arg, ITypeTuple<RS, Immediate>>(...);
    
    would handle an instruction that looks like:
    
      mnem $at,0x555
    
    The first "Arg" in the template parameter list generates code to read register RS from the operands list ($at). The second "Arg" generates code to read the Immediate from the operands list (0x555).
    
    Previously, without the sentinel, we had to check if an argument was provided for every kind of argument for the corresponding instruction format type, even if the instruction we were defining could never semantically receive that argument as a user-specified value.

--- lib/Assembler/MipsAssemblerTarget.cpp
+++ lib/Assembler/MipsAssemblerTarget.cpp
@@ -156,37 +156,47 @@ std::tuple<Target> JTypeTuple(std::string operand_str) {
     return tup;
 }
 
+//typedef uint32_t Field;
+constexpr uint32_t Arg = std::numeric_limits<uint32_t>::max();
+// TODO: assert all field types have fewer bits than Field
+
+constexpr bool IsArgSentinel(uint32_t field) {
+    return field == Arg;
+}
+
 typedef std::tuple<RS, RT, RD, Shift> (*RTypeSyntaxFunc)(std::string);
 
 template <uint32_t OpCode, uint32_t RS, uint32_t RT, uint32_t RD, uint32_t Shift, uint32_t FuncCode, RTypeSyntaxFunc Syntax>
 Instruction RType(const Entry& entry) {
-    constexpr uint32_t Data = (OpCode << 26U) | (RS << 21U) | (RT << 16U) | (RD << 11U) | (Shift << 6U) | FuncCode;
-
     Instruction instruction {};
-    instruction.data = Data;
+    instruction.data = OpCode << 26U | FuncCode;
     instruction.size = 4;
 
     auto operands = Syntax(entry.operands.value());
 
-    if (std::get<assembler::RS>(operands)) {
-        assert(RS == 0);
+    if constexpr (IsArgSentinel(RS)) {
         instruction.data |= ParseRegister(std::get<assembler::RS>(operands).value()) << 21U;
+    } else {
+        instruction.data |= RS << 21U;
     }
 
-    if (std::get<assembler::RT>(operands)) {
-        assert(RT == 0);
+    if constexpr (IsArgSentinel(RT)) {
         instruction.data |= ParseRegister(std::get<assembler::RT>(operands).value()) << 16U;
+    } else {
+        instruction.data |= RT << 16U;
     }
 
-    if (std::get<assembler::RD>(operands)) {
-        assert(RD == 0);
+    if constexpr (IsArgSentinel(RD)) {
         instruction.data |= ParseRegister(std::get<assembler::RD>(operands).value()) << 11U;
+    } else {
+        instruction.data |= RD << 11U;
     }
 
-    if (std::get<assembler::Shift>(operands)) {
-        assert(Shift == 0);
+    if constexpr (IsArgSentinel(Shift)) {
         instruction.expr_mappings.emplace_back(
             ExpressionMapping{ 6, 5, ParseExpression(std::get<assembler::Shift>(operands).value())});
+    } else {
+        instruction.data |= Shift << 6U;
     }
 
     return instruction;
@@ -196,28 +206,29 @@ typedef std::tuple<RS, RT, Immediate> (*ITypeSyntaxFunc)(std::string);
 
 template <uint32_t OpCode, uint32_t RS, uint32_t RT, uint32_t Immediate, ITypeSyntaxFunc Syntax>
 Instruction IType(const Entry& entry) {
-    constexpr uint32_t Data = (OpCode << 26U) | (RS << 21U) | (RT << 16U) | Immediate;
-
     Instruction instruction {};
-    instruction.data = Data;
+    instruction.data = OpCode << 26U;
     instruction.size = 4;
 
     auto operands = Syntax(entry.operands.value());
 
-    if (std::get<assembler::RS>(operands)) {
-        assert(RS == 0);
-        instruction.data = ParseRegister(std::get<assembler::RS>(operands).value()) << 21U;
+    if constexpr (IsArgSentinel(RS)) {
+        instruction.data |= ParseRegister(std::get<assembler::RS>(operands).value()) << 21U;
+    } else {
+        instruction.data |= RS << 21U;
     }
 
-    if (std::get<assembler::RT>(operands)) {
-        assert(RT == 0);
-        instruction.data = ParseRegister(std::get<assembler::RT>(operands).value()) << 16U;
+    if constexpr (IsArgSentinel(RT)) {
+        instruction.data |= ParseRegister(std::get<assembler::RT>(operands).value()) << 16U;
+    } else {
+        instruction.data |= RT << 16U;
     }
 
-    if (std::get<assembler::Immediate>(operands)) {
-        assert(Immediate == 0);
+    if constexpr (IsArgSentinel(Immediate)) {
         instruction.expr_mappings.emplace_back(
             ExpressionMapping{ 0, 16, ParseExpression(std::get<assembler::Immediate>(operands).value()) });
+    } else {
+        instruction.data |= Immediate;
     }
 
     return instruction;
@@ -227,17 +238,16 @@ typedef std::tuple<Target> (*JTypeSyntaxFunc)(std::string);
 
 template <uint32_t OpCode, uint32_t Target, JTypeSyntaxFunc Syntax>
 Instruction JType(const Entry& entry) {
-    constexpr uint32_t Data = (OpCode << 26U) | Target;
-
     Instruction instruction {};
-    instruction.data = Data;
+    instruction.data = OpCode << 26U;
     instruction.size = 4;
 
     auto operands = Syntax(entry.operands.value());
-    if (std::get<assembler::Target>(operands)) {
-        assert(Target == 0);
+    if constexpr (IsArgSentinel(Target)) {
         instruction.expr_mappings.emplace_back(
             ExpressionMapping{ 0, 26, ParseExpression(std::get<assembler::Target>(operands).value()) });
+    } else {
+        instruction.data |= Target;
     }
 
     return instruction;
@@ -245,11 +255,12 @@ Instruction JType(const Entry& entry) {
 
 Instruction ParseJALR(const Entry& entry) {
     try {
-        RType<0b000000, 0, 0b00000, 0, 0b000000, 0b001001, RTypeTuple<RD, RS>>(entry);
+        // TODO: catch correct exception only
+        RType<0b000000, Arg, 0b00000, Arg, 0b000000, 0b001001, RTypeTuple<RD, RS>>(entry);
     } catch (...) {
         // Try to parse as single register. If it works (it's RS), inject default $31 for RD.
         ParseRegister(entry.operands.value_or(""));
-        RType<0b000000, 0, 0b00000, 0, 0b000000, 0b001001, RTypeTuple<RD, RS>>(
+        RType<0b000000, Arg, 0b00000, Arg, 0b000000, 0b001001, RTypeTuple<RD, RS>>(
             Entry{
                 entry.label,
                 entry.operation,
@@ -270,92 +281,92 @@ Instruction ThrowInvalidCoprocessor(const Entry& entry) {
 
 typedef Instruction (*ParseFunc)(const Entry&);
 std::map<std::string, ParseFunc> instructions_fn = {
-    { "add",    RType<0b000000, 0, 0, 0, 0, 0b100000, RTypeTuple<RD, RS, RT>> },
-    { "addi",   IType<0b001000, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "addiu",  IType<0b001001, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "addu",   RType<0b000000, 0, 0, 0, 0, 0b100001, RTypeTuple<RD, RS, RT>> },
-    { "and",    RType<0b000000, 0, 0, 0, 0, 0b100100, RTypeTuple<RD, RS, RT>> },
-    { "andi",   IType<0b001100, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "beq",    IType<0b000100, 0, 0, 0, ITypeTuple<RS, RT, Immediate>> },
-    { "bgez",   IType<0b000001, 0, 0b00001, 0, ITypeTuple<RS, Immediate>> },
-    { "bgezal", IType<0b000001, 0, 0b10001, 0, ITypeTuple<RS, Immediate>> },
-    { "bgtz",   IType<0b000111, 0, 0b00000, 0, ITypeTuple<RS, Immediate>> },
-    { "blez",   IType<0b000110, 0, 0b00000, 0, ITypeTuple<RS, Immediate>> },
-    { "bltz",   IType<0b000001, 0, 0b00000, 0, ITypeTuple<RS, Immediate>> },
-    { "bltzal", IType<0b000001, 0, 0b10000, 0, ITypeTuple<RS, Immediate>> },
-    { "bne",    IType<0b000101, 0, 0, 0, ITypeTuple<RS, RT, Immediate>> },
-    { "break",  RType<0b000000, 0, 0, 0, 0, 0b001101, RTypeNoArgs> },
+    { "add",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100000, RTypeTuple<RD, RS, RT>> },
+    { "addi",   IType<0b001000, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "addiu",  IType<0b001001, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "addu",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100001, RTypeTuple<RD, RS, RT>> },
+    { "and",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100100, RTypeTuple<RD, RS, RT>> },
+    { "andi",   IType<0b001100, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "beq",    IType<0b000100, Arg, Arg, Arg, ITypeTuple<RS, RT, Immediate>> },
+    { "bgez",   IType<0b000001, Arg, 0b00001, Arg, ITypeTuple<RS, Immediate>> },
+    { "bgezal", IType<0b000001, Arg, 0b10001, Arg, ITypeTuple<RS, Immediate>> },
+    { "bgtz",   IType<0b000111, Arg, 0b00000, Arg, ITypeTuple<RS, Immediate>> },
+    { "blez",   IType<0b000110, Arg, 0b00000, Arg, ITypeTuple<RS, Immediate>> },
+    { "bltz",   IType<0b000001, Arg, 0b00000, Arg, ITypeTuple<RS, Immediate>> },
+    { "bltzal", IType<0b000001, Arg, 0b10000, Arg, ITypeTuple<RS, Immediate>> },
+    { "bne",    IType<0b000101, Arg, Arg, Arg, ITypeTuple<RS, RT, Immediate>> },
+    { "break",  RType<0b000000, 0b00000, 0b00000, 0b00000, 0b00000, 0b001101, RTypeNoArgs> },
     { "cfc0",   ThrowInvalidCoprocessor },
-    { "cfc1",   RType<0b010001, 0b00010, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "cfc2",   RType<0b010010, 0b00010, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "cfc3",   RType<0b010011, 0b00010, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "cfc1",   RType<0b010001, 0b00010, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "cfc2",   RType<0b010010, 0b00010, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "cfc3",   RType<0b010011, 0b00010, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
     { "cop0",   ThrowUnimplemented },
     { "cop1",   ThrowUnimplemented },
     { "cop2",   ThrowUnimplemented },
     { "cop3",   ThrowUnimplemented },
     { "ctc0",   ThrowInvalidCoprocessor },
-    { "ctc1",   RType<0b010001, 0b00110, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "ctc2",   RType<0b010010, 0b00110, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "ctc3",   RType<0b010011, 0b00110, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "div",    RType<0b000000, 0, 0, 0b00000, 0b000000, 0b011010, RTypeTuple<RS, RT>> },
-    { "divu",   RType<0b000000, 0, 0, 0b00000, 0b000000, 0b011011, RTypeTuple<RS, RT>> },
-    { "j",      JType<0b000010, 0, JTypeTuple<Target>> },
-    { "jal",    JType<0b000011, 0, JTypeTuple<Target>> },
+    { "ctc1",   RType<0b010001, 0b00110, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "ctc2",   RType<0b010010, 0b00110, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "ctc3",   RType<0b010011, 0b00110, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "div",    RType<0b000000, Arg, Arg, 0b00000, 0b000000, 0b011010, RTypeTuple<RS, RT>> },
+    { "divu",   RType<0b000000, Arg, Arg, 0b00000, 0b000000, 0b011011, RTypeTuple<RS, RT>> },
+    { "j",      JType<0b000010, Arg, JTypeTuple<Target>> },
+    { "jal",    JType<0b000011, Arg, JTypeTuple<Target>> },
     { "jalr",   ParseJALR /* special handling due to optional RD (defaults to $31)*/ },
-    { "jr",     RType<0b000000, 0, 0b00000, 0b00000, 0b000000, 0b001000, RTypeTuple<RS>> },
-    { "lb",     IType<0b100000, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lbu",    IType<0b100100, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lh",     IType<0b100001, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lhu",    IType<0b100101, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lui",    IType<0b001111, 0b00000, 0, 0, ITypeTuple<RT, Immediate>> },
-    { "lw",     IType<0b100011, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
+    { "jr",     RType<0b000000, Arg, 0b00000, 0b00000, 0b000000, 0b001000, RTypeTuple<RS>> },
+    { "lb",     IType<0b100000, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lbu",    IType<0b100100, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lh",     IType<0b100001, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lhu",    IType<0b100101, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lui",    IType<0b001111, 0b00000, Arg, Arg, ITypeTuple<RT, Immediate>> },
+    { "lw",     IType<0b100011, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
     { "lwc0",   ThrowInvalidCoprocessor },
-    { "lwc1",   IType<0b110001, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lwc2",   IType<0b110010, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lwc3",   IType<0b110011, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lwl",    IType<0b100010, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "lwr",    IType<0b100110, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "mfc0",   RType<0b010000, 0b00000, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mfc1",   RType<0b010001, 0b00000, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mfc2",   RType<0b010010, 0b00000, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mfc3",   RType<0b010011, 0b00000, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mfhi",   RType<0b000000, 0b00000, 0b00000, 0, 0b00000, 0b010000, RTypeTuple<RD>> },
-    { "mflo",   RType<0b000000, 0b00000, 0b00000, 0, 0b00000, 0b010010, RTypeTuple<RD>> },
-    { "mtc0",   RType<0b010000, 0b00100, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mtc1",   RType<0b010001, 0b00100, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mtc2",   RType<0b010010, 0b00100, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mtc3",   RType<0b010011, 0b00100, 0, 0, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
-    { "mthi",   RType<0b000000, 0, 0b00000, 0b00000, 0b00000, 0b010001, RTypeTuple<RS>> },
-    { "mtlo",   RType<0b000000, 0, 0b00000, 0b00000, 0b00000, 0b010011, RTypeTuple<RS>> },
-    { "mult",   RType<0b000000, 0, 0, 0b00000, 0b00000, 0b011000, RTypeTuple<RS, RT>> },
-    { "multu",  RType<0b000000, 0, 0, 0b00000, 0b00000, 0b011001, RTypeTuple<RS, RT>> },
-    { "nor",    RType<0b000000, 0, 0, 0, 0b00000, 0b100111, RTypeTuple<RD, RS, RT>> },
-    { "or",     RType<0b000000, 0, 0, 0, 0b00000, 0b100101, RTypeTuple<RD, RS, RT>> },
-    { "ori",    IType<0b001101, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "sb",     IType<0b101000, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "sh",     IType<0b101001, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "sll",    RType<0b000000, 0b00000, 0, 0, 0, 0b000000, RTypeTuple<RD, RT, Shift>> },
-    { "sllv",   RType<0b000000, 0, 0, 0, 0b00000, 0b000100, RTypeTuple<RD, RT, RS>> },
-    { "slt",    RType<0b000000, 0, 0, 0, 0b00000, 0b101010, RTypeTuple<RD, RS, RT>> },
-    { "slti",   IType<0b001010, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "sltiu",  IType<0b001011, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
-    { "sltu",   RType<0b000000, 0, 0, 0, 0b00000, 0b101011, RTypeTuple<RD, RS, RT>> },
-    { "sra",    RType<0b000000, 0b00000, 0, 0, 0, 0b000011, RTypeTuple<RD, RT, Shift>> },
-    { "srav",   RType<0b000000, 0, 0, 0, 0b00000, 0b000111, RTypeTuple<RD, RT, RS>> },
-    { "srl",    RType<0b000000, 0b00000, 0, 0, 0, 0b000010, RTypeTuple<RD, RT, Shift>> },
-    { "srlv",   RType<0b000000, 0, 0, 0, 0b00000, 0b000110, RTypeTuple<RD, RT, RS>> },
-    { "sub",    RType<0b000000, 0, 0, 0, 0b00000, 0b100010, RTypeTuple<RD, RS, RT>> },
-    { "subu",   RType<0b000000, 0, 0, 0, 0b00000, 0b100011, RTypeTuple<RD, RS, RT>> },
-    { "sw",     IType<0b101011, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
+    { "lwc1",   IType<0b110001, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lwc2",   IType<0b110010, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lwc3",   IType<0b110011, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lwl",    IType<0b100010, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "lwr",    IType<0b100110, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "mfc0",   RType<0b010000, 0b00000, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mfc1",   RType<0b010001, 0b00000, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mfc2",   RType<0b010010, 0b00000, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mfc3",   RType<0b010011, 0b00000, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mfhi",   RType<0b000000, 0b00000, 0b00000, Arg, 0b00000, 0b010000, RTypeTuple<RD>> },
+    { "mflo",   RType<0b000000, 0b00000, 0b00000, Arg, 0b00000, 0b010010, RTypeTuple<RD>> },
+    { "mtc0",   RType<0b010000, 0b00100, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mtc1",   RType<0b010001, 0b00100, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mtc2",   RType<0b010010, 0b00100, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mtc3",   RType<0b010011, 0b00100, Arg, Arg, 0b00000, 0b000000, RTypeTuple<RT, RD>> },
+    { "mthi",   RType<0b000000, Arg, 0b00000, 0b00000, 0b00000, 0b010001, RTypeTuple<RS>> },
+    { "mtlo",   RType<0b000000, Arg, 0b00000, 0b00000, 0b00000, 0b010011, RTypeTuple<RS>> },
+    { "mult",   RType<0b000000, Arg, Arg, 0b00000, 0b00000, 0b011000, RTypeTuple<RS, RT>> },
+    { "multu",  RType<0b000000, Arg, Arg, 0b00000, 0b00000, 0b011001, RTypeTuple<RS, RT>> },
+    { "nor",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100111, RTypeTuple<RD, RS, RT>> },
+    { "or",     RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100101, RTypeTuple<RD, RS, RT>> },
+    { "ori",    IType<0b001101, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "sb",     IType<0b101000, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "sh",     IType<0b101001, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "sll",    RType<0b000000, 0b00000, Arg, Arg, Arg, 0b000000, RTypeTuple<RD, RT, Shift>> },
+    { "sllv",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b000100, RTypeTuple<RD, RT, RS>> },
+    { "slt",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b101010, RTypeTuple<RD, RS, RT>> },
+    { "slti",   IType<0b001010, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "sltiu",  IType<0b001011, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
+    { "sltu",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b101011, RTypeTuple<RD, RS, RT>> },
+    { "sra",    RType<0b000000, 0b00000, Arg, Arg, Arg, 0b000011, RTypeTuple<RD, RT, Shift>> },
+    { "srav",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b000111, RTypeTuple<RD, RT, RS>> },
+    { "srl",    RType<0b000000, 0b00000, Arg, Arg, Arg, 0b000010, RTypeTuple<RD, RT, Shift>> },
+    { "srlv",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b000110, RTypeTuple<RD, RT, RS>> },
+    { "sub",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100010, RTypeTuple<RD, RS, RT>> },
+    { "subu",   RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100011, RTypeTuple<RD, RS, RT>> },
+    { "sw",     IType<0b101011, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
     { "swc0",   ThrowInvalidCoprocessor },
-    { "swc1",   IType<0b111001, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "swc2",   IType<0b111010, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "swc3",   IType<0b111011, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "swl",    IType<0b101010, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
-    { "swr",    IType<0b101110, 0, 0, 0, ITypeOffset<RT, Immediate, RS>> },
+    { "swc1",   IType<0b111001, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "swc2",   IType<0b111010, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "swc3",   IType<0b111011, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "swl",    IType<0b101010, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
+    { "swr",    IType<0b101110, Arg, Arg, Arg, ITypeOffset<RT, Immediate, RS>> },
     { "syscall",RType<0b000000, 0b00000, 0b00000, 0b00000, 0b00000, 0b001100, RTypeNoArgs> },
-    { "xor",    RType<0b000000, 0, 0, 0, 0b00000, 0b100110, RTypeTuple<RD, RS, RT>> },
-    { "xori",   IType<0b001110, 0, 0, 0, ITypeTuple<RT, RS, Immediate>> },
+    { "xor",    RType<0b000000, Arg, Arg, Arg, 0b00000, 0b100110, RTypeTuple<RD, RS, RT>> },
+    { "xori",   IType<0b001110, Arg, Arg, Arg, ITypeTuple<RT, RS, Immediate>> },
 
 };
 
