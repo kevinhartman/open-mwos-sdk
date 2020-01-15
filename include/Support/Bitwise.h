c0a8e87ec7fb7e98dc4aec247d03a998b8c677e6
Kevin Hartman <kevin@hart.mn>
2020-01-14 23:01:46 -0500

    [Support] Add inline keyword to helper functions.

--- include/Support/Bitwise.h
+++ include/Support/Bitwise.h
@@ -4,7 +4,7 @@
 
 namespace support {
 
-uint32_t RoundUpToNextPow2(uint32_t input) {
+inline uint32_t RoundUpToNextPow2(uint32_t input) {
     input--;
     input |= input >> 1;
     input |= input >> 2;
@@ -16,11 +16,11 @@ uint32_t RoundUpToNextPow2(uint32_t input) {
     return input;
 }
 
-bool IsPow2(uint32_t input) {
+inline bool IsPow2(uint32_t input) {
     return input > 0 && !(input & (input - 1));
 }
 
-size_t RoundToNextPow2Multiple(size_t input, size_t pow_2_multiple)
+inline size_t RoundToNextPow2Multiple(size_t input, size_t pow_2_multiple)
 {
     assert(IsPow2(pow_2_multiple));
     return (input + pow_2_multiple - 1) & -pow_2_multiple;
