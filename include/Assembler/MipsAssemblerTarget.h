7e705771158d59e6487f497c9f46dcffa132193c
Kevin Hartman <kevin@hart.mn>
2020-04-15 23:19:25 -0400

    Add missing explicit keyword to MIPS assembler target constructor.

--- include/Assembler/MipsAssemblerTarget.h
+++ include/Assembler/MipsAssemblerTarget.h
@@ -9,8 +9,8 @@ namespace assembler {
 
 class MipsAssemblerTarget : public AssemblerTarget {
 public:
-    MipsAssemblerTarget(support::Endian endianness);
-    virtual ~MipsAssemblerTarget() override;
+    explicit MipsAssemblerTarget(support::Endian endianness);
+    ~MipsAssemblerTarget() override;
     support::Endian GetEndianness() override;
     Instruction EmitInstruction(const Entry& entry) override;
 
