5372126ccd0906ba8d3a09c589e510829e55c422
Kevin Hartman <kevin@hart.mn>
2020-04-15 23:20:55 -0400

    Fix bugs with StringUtil Split.
    - Add anonymous namespace to avoid conflicts.
    - Add explicit early return check of empty string, which isn't handled properly by sregex_token_iterator split method.

--- include/Support/StringUtil.h
+++ include/Support/StringUtil.h
@@ -4,15 +4,23 @@
 #include <regex>
 #include <vector>
 
-std::vector<std::string> Split(const std::string& s, std::regex delimiter) {
+namespace {
+std::vector<std::string> Split(const std::string& s, const std::regex& delimiter) {
+    std::vector<std::string> elements {};
+
+    // sregex_token_iterator splitter usage doesn't seem to handle empty string properly.
+    if (s == "") {
+        return elements;
+    }
+
     std::sregex_token_iterator iter(s.begin(), s.end(), delimiter, -1);
     std::sregex_token_iterator end;
 
-    std::vector<std::string> elements {};
     while (iter != end)  {
         elements.push_back(*iter);
         ++iter;
     }
 
     return elements;
+}
 }
