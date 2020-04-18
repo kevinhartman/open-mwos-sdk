bb71f021fdaf14352077613f38ba6b873f7bcb15
Kevin Hartman <kevin@hart.mn>
2020-04-18 15:33:07 -0400

    Split DS tests into two scenarios.

--- test/Assembler/TestAssemblerPseudoInstHandler.cpp
+++ test/Assembler/TestAssemblerPseudoInstHandler.cpp
@@ -46,68 +46,88 @@ inline AssemblerOperationExceptionCode IsOperationFailureReason(OperationExcepti
 }
 
 namespace {
-    Entry ParseEntry(const std::string& input_line) {
-        std::stringstream input{};
-        input << input_line << std::endl;
+Entry ParseEntry(const std::string& input_line) {
+    std::stringstream input{};
+    input << input_line << std::endl;
 
-        assembler::InputFileParser parser {};
-        parser.Parse(input);
+    assembler::InputFileParser parser {};
+    parser.Parse(input);
 
-        return parser.GetListing().front();
+    return parser.GetListing().front();
+}
+}
+SCENARIO("DS operation expression resolution behavior", "[assembler]") {
+    AssemblerPseudoInstHandler handler {};
+
+    GIVEN("state in a vsect and psect context") {
+        AssemblyState state {};
+        state.in_psect = true;
+        state.in_vsect = true;
+
+        WHEN("the size operand is a constant expression") {
+            auto entry = ParseEntry("var ds.b 4+5/2");
+
+            REQUIRE(handler.Handle(entry, state));
+            REQUIRE(state.result->counter.uninitialized_data == 6);
+            REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
+        }
+        AND_WHEN("the size operand is an expression involving an external reference") {
+            auto entry = ParseEntry("var ds.b external+1");
+
+            REQUIRE_THROWS_AS(handler.Handle(entry, state), OperandException);
+        }
+        AND_WHEN("the size operand is an expression involving a constant expression EQU") {
+            OperandInfo info {};
+            info.op = "equ";
+            info.operand = "5+2";
+
+            state.psect.equs["constequ"] = std::make_unique<ExpressionOperand>(info);
+
+            auto entry = ParseEntry("var ds.b constequ+1");
+            REQUIRE(handler.Handle(entry, state));
+            REQUIRE(state.result->counter.uninitialized_data == 8);
+            REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
+        }
+        AND_WHEN("the size operand is an expression involving an EQU that references an external name") {
+            OperandInfo info {};
+            info.op = "equ";
+            info.operand = "5+extern";
+
+            state.psect.equs["equ"] = std::make_unique<ExpressionOperand>(info);
+
+            auto entry = ParseEntry("var ds.b equ+1");
+            REQUIRE_THROWS_AS(handler.Handle(entry, state), OperandException);
+        }
     }
 }
 
-    SCENARIO("DS operations work", "[assembler]") {
-        AssemblerPseudoInstHandler handler {};
+SCENARIO("DS operation behavior", "[assembler]") {
+    AssemblerPseudoInstHandler handler {};
 
-        GIVEN("state in a vsect and psect context") {
-            AssemblyState state {};
-            state.in_psect = true;
-            state.in_vsect = true;
+    GIVEN("state in a vsect and psect context") {
+        AssemblyState state {};
+        state.in_psect = true;
+        state.in_vsect = true;
 
-            WHEN("a single byte is declared with the label 'var'") {
-                auto entry = ParseEntry("var ds.b 1");
+        WHEN("a single byte is declared with the label 'var'") {
+            auto entry = ParseEntry("var ds.b 1");
 
-                REQUIRE(handler.Handle(entry, state));
-                REQUIRE(state.result->counter.uninitialized_data == 1);
-                REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
-            }
-            AND_WHEN("two bytes are declared with the label 'var'") {
-                auto entry = ParseEntry("var ds.b 2");
+            REQUIRE(handler.Handle(entry, state));
+            REQUIRE(state.result->counter.uninitialized_data == 1);
+            REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
+        }
+        AND_WHEN("two bytes are declared with the label 'var'") {
+            auto entry = ParseEntry("var ds.b 2");
 
-                REQUIRE(handler.Handle(entry, state));
-                REQUIRE(state.result->counter.uninitialized_data == 2);
-                REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
-            }
-            AND_WHEN("<constant expression> bytes are declared with the label 'var'") {
-                auto entry = ParseEntry("var ds.b 4+5/2");
+            REQUIRE(handler.Handle(entry, state));
+            REQUIRE(state.result->counter.uninitialized_data == 2);
+            REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
+        }
+        AND_WHEN("0 bytes are declared with the label 'var'") {
+            auto entry = ParseEntry("var ds.b 0");
 
-                REQUIRE(handler.Handle(entry, state));
-                REQUIRE(state.result->counter.uninitialized_data == 6);
-                REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
-            }
-            AND_WHEN("0 bytes are declared with the label 'var'") {
-                auto entry = ParseEntry("var ds.b 0");
-
-                REQUIRE_THROWS_AS(handler.Handle(entry, state), OperandException);
-            }
-            AND_WHEN("<constant expression w/ extern ref> bytes are declared with the label 'var'") {
-                auto entry = ParseEntry("var ds.b external+1");
-
-                REQUIRE_THROWS_AS(handler.Handle(entry, state), OperandException);
-            }
-            AND_WHEN("<constant expression w/ local equ ref> bytes are declared with the label 'var'") {
-                OperandInfo info {};
-                info.op = "equ";
-                info.operand = "5+2";
-
-                state.psect.equs["constequ"] = std::make_unique<ExpressionOperand>(info);
-
-                auto entry = ParseEntry("var ds.b constequ+1");
-                REQUIRE(handler.Handle(entry, state));
-                REQUIRE(state.result->counter.uninitialized_data == 8);
-                REQUIRE(state.GetSymbol("var") == object::SymbolInfo { object::SymbolInfo::Type::UninitData, false, 0 });
-            }
+            REQUIRE_THROWS_AS(handler.Handle(entry, state), OperandException);
         }
     }
+}
 }
