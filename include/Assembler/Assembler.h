7ce00d8a3cc72fec22743e237377dd9cdeb4394e
Kevin Hartman <kevin@hart.mn>
2020-05-19 23:46:08 -0400

    Include operation and operand index in exception messages.

--- include/Assembler/Assembler.h
+++ include/Assembler/Assembler.h
@@ -49,22 +49,33 @@ struct OperationException : std::runtime_error {
     };
 
     OperationException(std::string op, Code code, const std::string& cause)
-        : runtime_error(cause), op(std::move(op)), code(code) {}
+        : op(std::move(op)), code(code), runtime_error(GetMessage(op, cause)) {}
 
     std::string op;
     Code code;
+
+private:
+    static std::string GetMessage(const std::string& op, const std::string& cause) {
+        return "Operation: '" + op + "', message: " + cause;
+    }
 };
 
 struct OperandException : std::runtime_error {
     OperandException(std::string op, std::size_t position, const std::string& cause)
-        : op(std::move(op)), position(position), runtime_error(cause) {}
+        : op(std::move(op)), position(position), runtime_error(GetMessage(op, position, cause)) {}
 
     OperandException(std::string op, std::size_t position, const std::runtime_error& cause)
-        : op(std::move(op)), position(position), runtime_error(cause.what()), internal_exception(cause) {}
+        : op(std::move(op)), position(position), runtime_error(GetMessage(op, position, cause.what())),
+        internal_exception(cause) {}
 
     std::optional<std::runtime_error> internal_exception {};
     std::string op;
     std::size_t position;
+
+private:
+    static std::string GetMessage(const std::string& op, std::size_t position, const std::string& cause) {
+        return "Operation: '" + op + "', position: " + std::to_string(position) + ", message: " + cause;
+    }
 };
 
 }
